b1b0a17669665c9e418482d0ea9e8fc3c908f74f
Monique Azevedo da Silva <monique.azevedo@BRRJOBITAIM2724.local>
2020-11-04 08:21:45 -0300

    feat: Cria template da home e mock dos components

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,25 @@
-import Main from 'components/Main'
+import Home from 'templates/Home'
 
-const Home = () => <Main />
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+export default function Index(props: any) {
+  return <Home {...props} />
+}
 
-export default Home
+// ATENÇÃO:
+// os métodos getStaticProps/getServerSideProps SÓ FUNCIONAM EM PAGES
+
+// getStaticProps => gerar estático em build time
+// getServerSideProps => gerar via ssr a cada request
+// getInitialProps => gerar via ssr a cada request
+export function getServerSideProps() {
+  // faz lógica
+  // pode ser buscar dados numa API
+  // fazer calculo|leitura de context
+
+  // retorno dos dados
+  return {
+    props: {
+      heading: 'Olha eu aqui!!!!! 1234 ssr'
+    }
+  }
+}
