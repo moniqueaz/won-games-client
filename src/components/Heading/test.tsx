b2cbe5f29c3c601a1faaafe2bee9de1bf783c51e
Monique Azevedo da Silva <monique.azevedo@BRRJOBITAIM2724.local>
2020-11-08 15:31:23 -0300

    feat: add font size huge no theme, heading e logo

--- src/components/Heading/test.tsx
+++ src/components/Heading/test.tsx
@@ -11,6 +11,14 @@ describe('<Heading />', () => {
     })
   })
 
+  it('should render a heading with a huge size', () => {
+    renderWithTheme(<Heading size="huge">Won Games</Heading>)
+
+    expect(screen.getByRole('heading', { name: /won games/i })).toHaveStyle({
+      'font-size': '5.2rem'
+    })
+  })
+
   it('should render a black heading when color is passed', () => {
     renderWithTheme(<Heading color="black">Won Games</Heading>)
     expect(screen.getByRole('heading', { name: /won games/i })).toHaveStyle({
