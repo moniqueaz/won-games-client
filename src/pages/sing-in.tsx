11361fb9b2f7cc38029d12d438dcdb3a3582580e
Monique Azevedo da Silva <monique.azevedo@BRRJOBITAIM2724.local>
2020-11-08 16:20:11 -0300

    feat: add route sing-in e sing-up

---
+++ src/pages/sing-in.tsx
@@ -0,0 +1,28 @@
+import Auth from 'templates/Auth'
+
+// ATENÇÃO:
+// remover any e definir os types
+const SingIn = () => (
+  <Auth title="Sign In">
+    <div>Sing In</div>
+  </Auth>
+)
+
+// ATENÇÃO:
+// os métodos getStaticProps/getServerSideProps SÓ FUNCIONAM EM PAGES
+
+// getStaticProps => gerar estático em build time
+// getServerSideProps => gerar via ssr a cada request
+// getInitialProps => gerar via ssr a cada request
+export function getServerSideProps() {
+  // faz lógica
+  // pode ser buscar dados numa API
+  // fazer calculo|leitura de context
+
+  // retorno dos dados
+  return {
+    props: {}
+  }
+}
+
+export default SingIn
