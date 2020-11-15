3a8fa3fef2d9b9755fc9d4f2eda7124ff75db7ac
Monique Azevedo da Silva <monique.azevedo@BRRJOBITAIM2724.local>
2020-11-15 20:16:29 -0300

    feat: adicionar cobertura minima no teste

--- jest.config.js
+++ jest.config.js
@@ -2,7 +2,20 @@ module.exports = {
   testEnvironment: 'jsdom',
   testPathIgnorePatterns: ['/node_modules/', '/.next/'],
   collectCoverage: true,
-  collectCoverageFrom: ['src/**/*.ts(x)?', '!src/**/stories.tsx'],
+  collectCoverageFrom: [
+    'src/**/*.ts(x)?',
+    '!src/**/stories.tsx',
+    '!src/pages/**/*.tsx',
+    '!src/styles/**/*.ts'
+  ],
+  coverageThreshold: {
+    './src/**': {
+      branches: 80,
+      functions: 80,
+      lines: 80,
+      statements: 80
+    }
+  },
   setupFilesAfterEnv: ['<rootDir>/.jest/setup.ts'],
   moduleDirectories: ['node_modules', 'src'],
   modulePaths: ['<rootDir>/src/', '<rootDir>/.jest']
