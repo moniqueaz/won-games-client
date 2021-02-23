b6b3127dc556facd293139dbd0c4b7dc8ad849fc
Monique Azevedo <moniqueaz.si@gmail.com>
2021-02-23 12:57:59 -0300

    feat: config apollo

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,23 +1,33 @@
+import { ApolloProvider } from '@apollo/client'
+import { ThemeProvider } from 'styled-components'
 import { AppProps } from 'next/app'
 import Head from 'next/head'
-import { ThemeProvider } from 'styled-components'
+
 import GlobalStyles from 'styles/global'
 import theme from 'styles/theme'
+import { useApollo } from 'utils/apollo'
 
-function MyApp({ Component, pageProps }: AppProps) {
+function App({ Component, pageProps }: AppProps) {
+  const client = useApollo(pageProps.initialApoloState)
+
   return (
-    <ThemeProvider theme={theme}>
-      <Head>
-        <title>Won Games</title>
-        <link rel="shortcut icon" href="/img/icon-512.png" />
-        <link rel="apple-touch-icon" href="/img/icon-512.png" />
-        <link rel="manifest" href="/manifest.json" />
-        <meta name="description" content="The best Game Stores in the world!" />
-      </Head>
-      <GlobalStyles />
-      <Component {...pageProps} />
-    </ThemeProvider>
+    <ApolloProvider client={client}>
+      <ThemeProvider theme={theme}>
+        <Head>
+          <title>Won Games</title>
+          <link rel="shortcut icon" href="/img/icon-512.png" />
+          <link rel="apple-touch-icon" href="/img/icon-512.png" />
+          <link rel="manifest" href="/manifest.json" />
+          <meta
+            name="description"
+            content="The best Game Stores in the world!"
+          />
+        </Head>
+        <GlobalStyles />
+        <Component {...pageProps} />
+      </ThemeProvider>
+    </ApolloProvider>
   )
 }
 
-export default MyApp
+export default App
